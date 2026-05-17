ae3d456d7dd1f767a2918c49ef68206abdb6fd4d
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-17 12:11:53 -0500

    complete ShowSelection

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,23 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection({ shows, selectedShow }) {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({
+  shows,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    /** I need an array with a list of shows, so I need to map */
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          className={"show" + (show === selectedShow ? "selected" : "")}
+          key={show.name}
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
