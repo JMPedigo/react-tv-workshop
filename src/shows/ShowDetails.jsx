11e210441ca3c6a9426c410ee44a262c25dcf40b
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-17 09:51:06 -0500

    complete ShowDetails

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  // I need to set selected episode as a state variable
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  // Guard case to prompt selection of a show
+  if (!show) {
+    return <p>Please select a show to learn more.</p>;
+  }
+
+  // Otherwise ...
+  // I need to return props from EpisodeList
+  // ... then from EpisodeDetails
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
